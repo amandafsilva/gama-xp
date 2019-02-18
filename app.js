32a1fe25c77272592815e82561e4a0d75d7004fd
Vitor Xavier <vitorcxavier@me.com>
2019-02-18 14:46:40 -0300

    Adicionado IP ao lead, gerador de numeros ao form.html

--- app.js
+++ app.js
@@ -10,6 +10,44 @@ document.addEventListener("DOMContentLoaded", event => {
     firebase.initializeApp(config);
     const app = firebase.app();
 });
+var userIp;
+function getIp(callback)
+{
+    function response(s)
+    {
+        callback(window.userip);
+
+        s.onload = s.onerror = null;
+        document.body.removeChild(s);
+    }
+
+    function trigger()
+    {
+        window.userip = false;
+
+        var s = document.createElement("script");
+        s.async = true;
+        s.onload = function() {
+            response(s);
+        };
+        s.onerror = function() {
+            response(s);
+        };
+
+        s.src = "https://l2.io/ip.js?var=userip";
+        document.body.appendChild(s);
+    }
+
+    if (/^(interactive|complete)$/i.test(document.readyState)) {
+        trigger();
+    } else {
+        document.addEventListener('DOMContentLoaded', trigger);
+    }
+}
+
+getIp(function (ip) {
+    userIp = ip;
+});
 
 function insertLead(){
     let lnome = document.getElementById('nome').value;
@@ -24,7 +62,7 @@ function insertLead(){
     if(lnome === "" || lsnome === "" || lemail === "" || ltel === "" || lempresa === "" || lsegmento === ""){
         alert("Todos os campos devem ser preenchidos!");
     }else{
-        firebase.database().ref('leads/').push({nome: lnome, snome: lsnome, email: lemail, tel: ltel, empresa: lempresa, segmento: lsegmento, data: stringTime}, function(error){
+        firebase.database().ref('leads/').push({nome: lnome, snome: lsnome, email: lemail, tel: ltel, empresa: lempresa, segmento: lsegmento, data: stringTime, ip: userIp}, function(error){
             if(error) alert("ERRO");
             else alert("Email cadastrado!");
         });
